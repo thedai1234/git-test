97b4b2d59b7593ee0f9b3999bc7b179f82f91404
thedai <thedai96@163.com>
2021-08-27 16:51:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * DATE: 2021/8/27
+ * Author: TheDai
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
